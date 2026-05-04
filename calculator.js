aa4ab55c1d5bfde4a59552cfd41d2ff2244fe8b2
vishal <vishal.reddy456456@gmail.com>
2026-05-04 14:27:24 +0530

    swithcase

--- calculator.js
+++ calculator.js
@@ -2,14 +2,14 @@ let n1=45,n2=56;
 let op='+'
 switch(op)
 {
-    case '+': console.log(n1+n2)
-                break
-    case '-': console.log(n1-n2)
-                break
-    case '*': console.log(n1*n2)
-                break
-    case '/': console.log(n1/n2)
-                break
-    case '%': console.log(n1%n2)
-                break
+    case '+': console.log(n1+n2);
+                break;
+    case '-': console.log(n1-n2);
+                break;
+    case '*': console.log(n1*n2);
+                break;
+    case '/': console.log(n1/n2);
+                break;
+    case '%': console.log(n1%n2);
+                break;
 }
