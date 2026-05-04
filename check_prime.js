958a865e0617f6aaa99f87cf0a27e387bdd2557a
vishal <vishal.reddy456456@gmail.com>
2026-05-04 15:25:19 +0530

    check_prime

--- check_prime.js
+++ check_prime.js
@@ -1,4 +1,12 @@
 function check_prime(n)
 {
-    
+    for(let i=2;i<n;i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
 }
+console.log(check_prime(15));
