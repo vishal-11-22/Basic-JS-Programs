beaf22e629b6d852aff26a8ec0bd56db5fd82f65
vishal <vishal.reddy456456@gmail.com>
2026-05-04 14:23:11 +0530

    switchcase

---
+++ calculator.js
@@ -0,0 +1,15 @@
+let n1=45,n2=56;
+let op='+'
+switch(op)
+{
+    case '+': console.log(n1+n2)
+                break
+    case '-': console.log(n1-n2)
+                break
+    case '*': console.log(n1*n2)
+                break
+    case '/': console.log(n1/n2)
+                break
+    case '%': console.log(n1%n2)
+                break
+}
